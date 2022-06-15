02e718323128bec1b39cd5b04732923c6fcc7d61
Viktor <ecartman41@gmail.com>
2022-06-15 16:39:11 +0300

    init: set inviroment

---
+++ src/store/counterSlice.ts
@@ -0,0 +1,13 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {},
+});
+
+export const {} = counterSlice.actions;
+
+export default counterSlice.reducer;
